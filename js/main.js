a8bca96abe94c2f62c4d3354e632adc88ddc32c4
nurgnc <gencbedriyenur@gmail.com>
2021-03-12 18:25:09 +0300

    completed reference section.

--- js/main.js
+++ js/main.js
@@ -8,3 +8,14 @@ $(".common-slider")
         autoplay: true,
         autoplaySpeed: 3000
     });
+// Reference Slider
+$(".common-slider-logo")
+    .not(".slick-intialized")
+    .slick({
+        prevArrow: ".slick-slider .prev",
+        nextArrow: ".slick-slider .next",
+        slidesToShow: 7,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000
+    });
