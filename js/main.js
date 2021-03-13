6164edb9993f15204224337f6fec6e514e7328c6
nurgnc <gencbedriyenur@gmail.com>
2021-03-13 16:24:18 +0300

    created brand slider.

--- js/main.js
+++ js/main.js
@@ -1,19 +1,42 @@
-$(".common-slider")
+// Brand Slider
+$(".brand-slider")
     .not(".slick-intialized")
     .slick({
-        prevArrow: ".slick-slider .prev",
-        nextArrow: ".slick-slider .next",
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 3000
+    });
+
+// Discount Slider
+$(".discount-slider")
+    .not(".slick-intialized")
+    .slick({
+        prevArrow: $('.prev-discount'),
+        nextArrow: $('.next-discount'),
         slidesToShow: 4,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 3000
     });
+
+// New Product Slider
+$(".new-slider")
+    .not(".slick-intialized")
+    .slick({
+        prevArrow: $('.prev-new'),
+        nextArrow: $('.next-new'),
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000
+    });
+
 // Reference Slider
 $(".common-slider-logo")
     .not(".slick-intialized")
     .slick({
-        prevArrow: ".slick-slider .prev",
-        nextArrow: ".slick-slider .next",
+        prevArrow: $('.prev'),
+        nextArrow: $('.next'),
         slidesToShow: 7,
         slidesToScroll: 1,
         autoplay: true,
